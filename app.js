fc7b827f800108811f80d8a3c1b7ad4fb031d04b
theBashShell <github.com/theBashShell>
2018-09-22 18:34:02 +0000

    page done

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/static'));
+
+
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+app.listen(3000);
