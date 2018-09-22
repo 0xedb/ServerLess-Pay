6b34eaa3dd33a223fa20954f848e045fb5c86172
theBashShell <github.com/theBashShell>
2018-09-22 19:26:58 +0000

    fixing

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/static'));
@@ -9,4 +10,4 @@ app.get('/', function(req, res) {
     res.render('pages/index');
 });
 
-app.listen(9000);
+app.listen(process.env.PORT || 9000);
