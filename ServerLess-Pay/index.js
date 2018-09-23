413e993cd4ec5e4c2c945f4045ecad8995df809e
theBashShell <github.com/theBashShell>
2018-09-23 03:08:02 +0000

    fixing fnc

--- ServerLess-Pay/index.js
+++ ServerLess-Pay/index.js
@@ -1,4 +1,4 @@
-const curl = new (require("curl-request"))();
+const https = require('https');
 const create_token_url = "https://paymentspring.com/api/v1/tokens";
 
 let param = {
@@ -9,13 +9,19 @@ let param = {
 "card_owner_name": "John Doe"
 };
 
+let options = {
+    method: 'POST',
+    headers: {
+        'Authorization': 'Basic ' + btoa(process.env.PUBLIC_KEY)
+    }, 
+    //auth: process.env.PUBLIC_KEY + ":"
+};
+
 module.exports = function (context, req) {
-    
-    curl.setHeaders(["Authorization: " + "Basic " + btoa(process.env.PUBLIC_KEY + ":")]).setBody(param).post(create_token_url).then(({statusCode, body, headers}) => {
-        context.log(body);
-    }).catch((e) => {
-        context.log(e);
-    });
+    https.write(param);
+    https.request(create_token_url, options);
+    https.on('end', () => context.log('Done'));
+    https.on('error', (e) => context.log(e));
 
 
     context.done();
