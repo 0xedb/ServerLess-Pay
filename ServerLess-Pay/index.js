e5d7865ba143085df9238c657d77949c775e817b
theBashShell <github.com/theBashShell>
2018-09-23 20:23:46 +0000

    working

--- ServerLess-Pay/index.js
+++ ServerLess-Pay/index.js
@@ -1,42 +1,50 @@
-const btoa = require('btoa');
-const https = require('https');
-const create_token_url = "https://paymentspring.com/api/v1/tokens";
+var qs = require("querystring");
+var http = require("https");
 
-let param = {
-"card_number": "4111111111111111",
-"card_exp_month": "1",
-"card_exp_year": "2020",
-"csc": "1234",
-"card_owner_name": "John Doe"
-};
-
-let options = {
-    method: 'POST',
-    headers: {
-        'Authorization': 'Basic ' + btoa(process.env.PUBLIC_KEY)
-    }, 
-    //auth: process.env.PUBLIC_KEY + ":"
-};
 
 module.exports = function (context, req) {
-    https.write(param);
-    let res = https.request(create_token_url, options);
-    https.on('end', () => context.log('Done'));
-    https.on('error', (e) => context.log(e));
+    var options = {
+        "method": "POST",
+        "hostname": [
+            "api",
+            "paymentspring",
+            "com"
+        ],
+        "path": [
+            "api",
+            "v1",
+            "tokens"
+        ],
+        "headers": {
+            "Authorization": "Basic dGVzdF82ZWY1ZTg0ZDFiODM4OTRlNGM4YmM1OGI3YzEwNTQ1OWNlMzBiNmQxNjM5MmQ1ZjAyZGQ5MmI0NDJiOg==",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Cache-Control": "no-cache",
+            "Postman-Token": "793262d0-9bcd-40b1-aa43-ed7f92143e1d"
+        }
+    };
+
+    var req = http.request(options, function(res) {
+        var chunks = [];
+
+        res.on("data", function(chunk) {
+            chunks.push(chunk);
+        });
+
+        res.on("end", function() {
+            var body = Buffer.concat(chunks);
+            console.log(body.toString());
+        });
+    });
+
+    req.write(qs.stringify({
+        card_number: '4111111111111111',
+        card_exp_month: '12',
+        card_exp_year: '2029',
+        csc: '999'
+    }));
+    req.end();
 
 
     context.done();
 };
-
-
-
-
-
-let param = {
-    "card_number": "4111111111111111",
-    "card_exp_month": "1",
-    "card_exp_year": "2020",
-    "csc": "1234",
-    "card_owner_name": "John Doe"
-};
-
+ 
