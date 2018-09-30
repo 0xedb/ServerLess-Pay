64178da4d2d5550f254162435a86f47821067737
theBashShell <edohbruno@gmail.com>
2018-09-30 22:24:36 +0000

    number formate corrected

--- Pay-Notify/index.js
+++ Pay-Notify/index.js
@@ -4,7 +4,9 @@ module.exports = function (context, req) {
     let data = req.body.payload.amount_settled;
     let dollar = data.slice(0, data.length - 2);
     let cent = data.slice(data.lenght - 2);
-    let amount = dollar + "." + cent;
-    context.log(amount);   
+    let amount = new Number(dollar + "." + cent); 
+
+    // Return amount to update the website 
+
     context.done();
 };
