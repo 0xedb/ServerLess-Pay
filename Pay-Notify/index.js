881fb2f8896f7c7de4afef8bf024a23731748c4d
theBashShell <edohbruno@gmail.com>
2018-10-02 18:30:01 +0000

    added 0

--- Pay-Notify/index.js
+++ Pay-Notify/index.js
@@ -3,10 +3,13 @@
 module.exports = function (context, req) { 
     let data = req.body.payload.amount_settled;
     let dollar = data.slice(0, data.length - 2);
-    let cent = data.slice(data.length - 2);
-    let amount = new Number(dollar + "." + cent); 
+    let cent = data.slice(data.lenght - 2);
+    let amount = dollar + "." + cent;  
 
-    // Return amount to update the website 
+    context.res = { 
+            body: amount
+        };  
 
+        
     context.done();
 };
