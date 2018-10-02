c482af34406a9491c01a8d7988ed7422ecb4154d
theBashShell <edohbruno@gmail.com>
2018-10-02 21:49:18 +0000

    fixed api

--- Pay-Notify/index.js
+++ Pay-Notify/index.js
@@ -1,15 +1,46 @@
+var qs = require("querystring");
+var http = require("http");
 
-
-module.exports = function (context, req) { 
-    let data = req.body.payload.amount_settled;
+module.exports = function (context, req) {
+    let data = req.body.payload.amount_settled; 
     let dollar = data.slice(0, data.length - 2);
     let cent = data.slice(data.lenght - 2);
-    let amount = dollar + "." + cent;  
+    let amount = dollar + "." + cent; 
 
-    context.res = { 
-            body: amount
-        };  
+    let api = "https://serverless-pay.herokuapp.com/api";
+    
+    context.res = {
+        amount: amount 
+    }; 
 
-        
+    // make post request to site
+    var options = {
+        "method": "POST",
+        "hostname": [
+          api
+        ], 
+        "headers": {
+          "Content-Type": "application/x-www-form-urlencoded",
+          "Cache-Control": "no-cache"
+        }
+      };
+      
+      var reqq = http.request(options, function (res) {
+        var chunks = [];
+      
+        res.on("data", function (chunk) {
+          chunks.push(chunk);
+        });
+      
+        res.on("end", function () {
+          var body = Buffer.concat(chunks);
+          console.log(body.toString());
+        });
+      });
+      
+      reqq.write(qs.stringify({ amount: amount }));
+      reqq.end();
+
+
     context.done();
 };
--- app.js
+++ app.js
@@ -1,17 +1,44 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/static'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+let amount = 10; 
 
 
 app.get('/', function (req, res) {
     res.render('pages/index');
 });
 
+// handle post request
+app.post('/api', function (req, res) { 
+    // perform security checks
+
+    amount = req.body.amount; 
+});
+
+app.get('/api', function(req, res) {
+    //perform security checks
+
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ 'amount': amount}));
+});
+
+
 app.get('*', function (req, res) {
     res.render('pages/index');
 });
 
+
 app.listen(process.env.PORT || 9090);
+
+
+module.exports = app; 
+ 
